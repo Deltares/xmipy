9fce57e7760ae97c15ffde6cdbe87f429c66dc6d
Hofer Julian <30049909+Hofer-Julian@users.noreply.github.com>
2020-07-01 08:25:36 +0200

    Convert fixture to simple test (#17)
    * Convert fixture to simple test
    
    * Remove uncessary imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,56 +1,14 @@
 import pytest
-import pymake
 import platform
-import os
 from pathlib import Path
 
 
 @pytest.fixture(scope="session")
-def get_shared_object(tmpdir_factory):
-
-    tmp_dir = tmpdir_factory.getbasetemp()
-    bin_dir = tmp_dir / "bin"
-    download_dir = tmp_dir / "download"
-
-    pymake.download_and_unzip(
-        url="https://github.com/MODFLOW-USGS/modflow6/archive/develop.zip",
-        pth=str(download_dir),
-    )
-
-    # set source and target paths
-    srcdir = download_dir / "modflow6-develop" / "srcbmi"
-    comdir = download_dir / "modflow6-develop" / "src"
-    excludefiles = [str(comdir / "mf6.f90")]
-    target = str(bin_dir / "libmf6")
-
-    # make sure the correct extension is used for each operating system
-    eext = ".so"
+def get_shared_object_path():
     sysinfo = platform.system()
     if sysinfo.lower() == "windows":
-        eext = ".dll"
-    target += eext
+        lib_name = "libmf6.dll"
+    else:
+        lib_name = "libmf6.so"
+    return str(Path(__file__).parent / "bin" / lib_name)
 
-    fc, cc = pymake.set_compiler("mf6")
-
-    fflags = None
-    if fc == "gfortran":
-        # some flags to check for errors in the code
-        # add -Werror for compilation to terminate if errors are found
-        fflags = (
-            "-Wtabs -Wline-truncation -Wunused-label "
-            "-Wunused-variable -pedantic -std=f2008"
-        )
-
-    pymake.main(
-        str(srcdir),
-        target,
-        fc=fc,
-        cc=cc,
-        include_subdirs=True,
-        fflags=fflags,
-        srcdir2=str(comdir),
-        excludefiles=excludefiles,
-        sharedobject=True,
-    )
-
-    return target
