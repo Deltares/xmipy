c44ce197e6d530c88c1893f7bbbcc991b9a748ac
Julian Hofer <julian.git@mailbox.org>
2020-07-10 14:03:24 +0200

    Make use of modflows new nightly release repo

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,23 +2,34 @@ import pytest
 import platform
 from pathlib import Path
 import flopy
+import pymake
 
 
 @pytest.fixture(scope="session")
-def modflow_lib_path():
+def modflow_lib_path(tmp_path_factory):
+    tmp_path = tmp_path_factory.getbasetemp()
+    url = "https://github.com/MODFLOW-USGS/modflow6-nightly-build/releases/latest/download/"
     sysinfo = platform.system()
-    if sysinfo.lower() == "windows":
-        lib_name = "libmf6.dll"
-    else:
-        lib_name = "libmf6.so"
-    return str(Path(__file__).parent / "bin" / lib_name)
+    if sysinfo == "Windows":
+        url += "win64.zip"
+        lib_path = tmp_path / "libmf6.dll"
+    elif sysinfo == "Linux":
+        url += "linux.zip"
+        lib_path = tmp_path / "libmf6.so"
+    elif sysinfo == "Darwin":
+        url += "mac.zip"
+        lib_path = tmp_path / "libmf6.so"
+
+    pymake.download_and_unzip(
+        url=url, pth=str(tmp_path),
+    )
+    return str(lib_path)
 
 
 @pytest.fixture(scope="function")
-def flopy_dis(tmp_path_factory):
+def flopy_dis(tmp_path):
     name = "test_model_dis"
-    tmp_dir = tmp_path_factory.mktemp(name)
-    sim = flopy.mf6.MFSimulation(sim_name=name, sim_ws=str(tmp_dir))
+    sim = flopy.mf6.MFSimulation(sim_name=name, sim_ws=str(tmp_path))
 
     tdis_rc = [(6.0, 2, 1.0), (6.0, 3, 1.0)]
     flopy.mf6.ModflowTdis(sim, time_units="DAYS", nper=2, perioddata=tdis_rc)
@@ -39,5 +50,5 @@ def flopy_dis(tmp_path_factory):
         saverecord=[("HEAD", "ALL"), ("BUDGET", "ALL")],
     )
     sim.write_simulation()
-    model_dir = str(tmp_dir)
+    model_dir = str(tmp_path)
     return (model_dir, sim)
