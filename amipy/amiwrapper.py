ab434fbeca1ffeac1fb8ef244b2879c41c2dff54
Julian Hofer <julian.git@mailbox.org>
2020-07-14 17:36:53 +0200

    Supress stdin and stdout

--- amipy/amiwrapper.py
+++ amipy/amiwrapper.py
@@ -443,6 +443,24 @@ def execute_function(function, arguments, detail=""):
     """
     Utility function to execute a BMI function in the kernel and checks its status
     """
-    if function(*arguments) != Status.SUCCESS:
-        msg = f"MODFLOW 6 BMI, exception in: {function.__name__} ({detail})"
-        raise Exception(msg)
+    with SuppressStream(sys.stdout):
+        with SuppressStream(sys.stderr):
+            if function(*arguments) != Status.SUCCESS:
+                msg = f"MODFLOW 6 BMI, exception in: {function.__name__} ({detail})"
+                raise Exception(msg)
+
+
+class SuppressStream:
+    def __init__(self, stream=sys.stderr):
+        self.orig_stream_fileno = stream.fileno()
+
+    def __enter__(self):
+        self.orig_stream_dup = os.dup(self.orig_stream_fileno)
+        self.devnull = open(os.devnull, "w")
+        os.dup2(self.devnull.fileno(), self.orig_stream_fileno)
+
+    def __exit__(self, type, value, traceback):
+        os.close(self.orig_stream_fileno)
+        os.dup2(self.orig_stream_dup, self.orig_stream_fileno)
+        os.close(self.orig_stream_dup)
+        self.devnull.close()
