9bb3f8ccf05136bfb6432b66032c6560cd6a71fc
Julian Hofer <julian.git@mailbox.org>
2020-07-21 16:34:31 +0200

    Extend README and improve setup.py

--- README.md
+++ README.md
@@ -4,4 +4,11 @@
 [![codecov](https://codecov.io/gh/Deltares/xmipy/branch/develop/graph/badge.svg)](https://codecov.io/gh/Deltares/xmipy)
 
 
-BMI python wrapper for MODFLOW 6 (and some more)
+`xmipy` is an extension to [bmipy](https://pypi.org/project/bmipy/) including an implementation of the abstract methods.
+The extended interface is required to couple certain hydrological kernels.
+The [imod_coupler](https://github.com/Deltares/imod_coupler) uses it to couple Modflow and Metaswap.
+
+`xmipy` can be installed by running
+```
+pip install xmipy
+```
--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 import sys
 from setuptools import find_namespace_packages, setup
-import os
-
-
-__version__ = "0.1.1"
-__pakname__ = "xmipy"
+import codecs
+import os.path
 
 # edit author dictionary as necessary
 author_dict = {
@@ -16,22 +13,41 @@ __author__ = ", ".join(author_dict.keys())
 __author_email__ = ", ".join(s for _, s in author_dict.items())
 
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+long_description = read("README.md")
+
+
 # ensure minimum version of Python is running
 if sys.version_info[0:2] < (3, 6):
     raise RuntimeError("Python >= 3.6 is required")
 
 
 setup(
-    name=__pakname__,
-    description=f"{__pakname__} is an extension to the bmipy Python package .",
-    long_description=f"{__pakname__} is an extension to the bmipy Python package "
-    + "for MODFLOW 6 and other codes.",
+    name="xmipy",
+    description="xmipy is an extension to the bmipy Python package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author=__author__,
+    author_email=__author_email__,
     url="https://github.com/Deltares/xmipy.git",
     license="CC0",
     platforms="Windows, Mac OS-X, Linux",
     install_requires=["bmipy", "numpy"],
     packages=find_namespace_packages(exclude=("tests", "examples")),
-    version=__version__,
+    version=get_version("xmipy/__init__.py"),
     classifiers=["Topic :: Scientific/Engineering :: Hydrology"],
 )
