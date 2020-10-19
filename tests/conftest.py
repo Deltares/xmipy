e5e8e9b66318593bcfd2676a91e88546dc4a5420
Martijn Russcher <Martijn.Russcher@deltares.nl>
2020-10-19 08:21:07 +0100

    - forgot to add the updated test config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,11 @@
 import pytest
 import platform
-from pathlib import Path
 import flopy
 import pymake
 from dataclasses import dataclass
 from typing import List, Tuple, Any
 from flopy.mf6 import MFSimulation
 import numpy as np
-import os
-import shutil
 
 
 @pytest.fixture(scope="session")
@@ -61,7 +58,9 @@ def flopy_dis(tmp_path, modflow_lib_path):
     flopy.mf6.ModflowTdis(sim, time_units="DAYS", nper=2, perioddata=flopy_dis.tdis_rc)
     flopy.mf6.ModflowIms(sim)
     gwf = flopy.mf6.ModflowGwf(sim, modelname=flopy_dis.model_name, save_flows=True)
-    flopy.mf6.ModflowGwfdis(gwf, nrow=flopy_dis.nrow, ncol=flopy_dis.ncol)
+    flopy.mf6.ModflowGwfdis(
+        gwf, nrow=flopy_dis.nrow, ncol=flopy_dis.ncol, delr=10.0, delc=10.0
+    )
     flopy.mf6.ModflowGwfic(gwf)
     flopy.mf6.ModflowGwfnpf(gwf, save_specific_discharge=True)
     flopy.mf6.ModflowGwfchd(gwf, stress_period_data=flopy_dis.stress_period_data)
@@ -253,3 +252,41 @@ def flopy_disu(tmp_path):
     flopy.mf6.ModflowGwfnpf(gwf, save_specific_discharge=True)
     sim.write_simulation()
     return flopy_disu
+
+
+@dataclass
+class FlopyGwfSto:
+    sim_path: str
+    sim: MFSimulation
+    model_name: str
+
+
+@pytest.fixture(scope="function")
+def flopy_gwf_sto(tmp_path, modflow_lib_path):
+    sim_path = str(tmp_path)
+    sim = flopy.mf6.MFSimulation(
+        sim_name="TEST_SIM_DIS", version="mf6", sim_ws=sim_path
+    )
+    flopy_gwf_sto = FlopyGwfSto(sim_path=sim_path, sim=sim, model_name="TEST_MODEL_STO")
+    flopy.mf6.ModflowTdis(
+        sim, time_units="DAYS", nper=2, perioddata=[(6.0, 2, 1.0), (6.0, 3, 1.0)]
+    )
+    flopy.mf6.ModflowIms(sim)
+    gwf = flopy.mf6.ModflowGwf(sim, modelname=flopy_gwf_sto.model_name, save_flows=True)
+    flopy.mf6.ModflowGwfdis(gwf, nrow=1, ncol=10, delr=10.0, delc=10.0)
+    flopy.mf6.ModflowGwfic(gwf)
+    flopy.mf6.ModflowGwfnpf(gwf, save_specific_discharge=True)
+    flopy.mf6.ModflowGwfchd(
+        gwf, stress_period_data=[[(0, 0, 0), 1.0], [(0, 0, 9), 0.0]]
+    )
+    flopy.mf6.ModflowGwfsto(gwf, ss=[1.0e-4] * 10, sy=[0.5e-1] * 10)
+    budget_file = flopy_gwf_sto.model_name + ".bud"
+    head_file = flopy_gwf_sto.model_name + ".hds"
+    flopy.mf6.ModflowGwfoc(
+        gwf,
+        budget_filerecord=budget_file,
+        head_filerecord=head_file,
+        saverecord=[("HEAD", "ALL"), ("BUDGET", "ALL")],
+    )
+    sim.write_simulation()
+    return flopy_gwf_sto
