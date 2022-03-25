b6927727f694f27df11036459d939e1ae35fb814
Hofer-Julian <julianhofer@gnome.org>
2022-03-25 09:39:58 +0100

    Export types

--- xmipy/__init__.py
+++ xmipy/__init__.py
@@ -1,5 +1,5 @@
 # imports
-from xmipy.xmi import Xmi
-from xmipy.xmiwrapper import XmiWrapper
+from xmipy.xmi import Xmi as Xmi
+from xmipy.xmiwrapper import XmiWrapper as XmiWrapper
 
 __version__ = "1.0.0"
