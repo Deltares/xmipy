6a75010d320294b71d63072b74dc7e65d4250132
mjr-deltares <45555666+mjr-deltares@users.noreply.github.com>
2023-04-04 11:12:19 +0200

    - add update_until (#94)
    - add initialize_mpi

--- xmipy/xmiwrapper.py
+++ xmipy/xmiwrapper.py
@@ -132,13 +132,23 @@ class XmiWrapper(Xmi):
                 self._state = State.INITIALIZED
         else:
             raise InputError("The library is already initialized")
+        
+    def initialize_mpi(self, value: int) -> None:
+        if self._state == State.UNINITIALIZED:
+            with cd(self.working_directory):
+                comm = c_int(value)
+                self._execute_function(self.lib.initialize_mpi, byref(comm))
+                self._state = State.INITIALIZED
+        else:
+            raise InputError("The library is already initialized")
 
     def update(self) -> None:
         with cd(self.working_directory):
             self._execute_function(self.lib.update)
 
     def update_until(self, time: float) -> None:
-        raise NotImplementedError
+        with cd(self.working_directory):
+            self._execute_function(self.lib.update_until, c_double(time))
 
     def finalize(self) -> None:
         if self._state == State.INITIALIZED:
